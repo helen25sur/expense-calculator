89c0a9ea7805a962ca297deecb5374d40025ac60
Elena Surilova <elen21surilova@gmail.com>
2023-05-03 23:31:35 +0300

    feat: add event handlers to inputs in the form
    and using useState for the form
    
    Course 'React - The Complete Guide (incl Hooks, React Router, Redux)'

--- src/components/NewExpense/ExpenseForm/ExpenseForm.tsx
+++ src/components/NewExpense/ExpenseForm/ExpenseForm.tsx
@@ -1,22 +1,62 @@
+import { ChangeEvent, useState } from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
+  const [userInput, setUserInput] = useState({
+    enteredTitle: '',
+    enteredAmount: '',
+    enteredDate: ''
+  });
+
+  const titleChangeHandler = (event: ChangeEvent) => {
+    const target = event.target as HTMLInputElement;
+    setUserInput((prevState) => {
+      return {
+        ...prevState,
+        enteredTitle: target.value,
+      }
+    });
+    console.log(userInput);
+  }
+
+  const amountChangeHandler = (event: ChangeEvent) => {
+    const target = event.target as HTMLInputElement;
+    setUserInput((prevState) => {
+      return {
+        ...prevState,
+        enteredAmount: target.value,
+      }
+    });
+    console.log(userInput);
+  }
+
+  const dateChangeHandler = (event: ChangeEvent) => {
+    const target = event.target as HTMLInputElement;
+    setUserInput((prevState) => {
+      return {
+        ...prevState,
+        enteredDate: target.value,
+      }
+    });
+    console.log(userInput);
+  }
+
   return (
     <form>
       <div className='new-expense_controls'>
         <div className='new-expense_control'>
           <label className='body-small'>Title:</label>
-          <input className='body-large' type='text' />
+          <input className='body-large' type='text' onChange={titleChangeHandler}/>
         </div>
 
         <div className='new-expense_control'>
           <label className='body-small'>Amount:</label>
-          <input className='body-large' type='number' min='0.01' step='0.01' />
+          <input className='body-large' type='number' min='0.01' step='0.01' onChange={amountChangeHandler}/>
         </div>
 
         <div className='new-expense_control'>
           <label className='body-small'>Date:</label>
-          <input className='body-large' type='date' defaultValue='2023-04-26' min='2020-01-01' max='2023-12-31'/>
+          <input className='body-large' type='date' defaultValue='2023-04-26' min='2020-01-01' max='2023-12-31' onChange={dateChangeHandler}/>
         </div>
       </div>
 
